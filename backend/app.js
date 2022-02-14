104193fd788a1e73dcebc525ef8101192616c512
chafi9-code <ch.tacherifte@gmail.com>
2022-02-14 01:48:48 +0100

    add new middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 
 const app = express();
-app.use((request, response) => {
-  response.json({message: 'request recived'});
+
+app.use((request, response, next) => {
+  console.log('request recived');
+  next();
+});
+
+app.use((request, response, next) => {
+  response.status(201);
+  next();
+});
+
+app.use((request, response, next) => {
+  response.json({message: 'request recived with success'});
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log('respons sent');
 });
 
 module.exports = app;
